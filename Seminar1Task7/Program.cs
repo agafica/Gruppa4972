15370d15fc40b67dc1310adc1c2233be85e14f53
Albina <sagafio@mail.ru>
2023-09-04 22:01:13 +0300

    solution from seminar1Task7

---
+++ Seminar1Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter the number: ");
+int num=int.Parse(Console.ReadLine()??"0");
+
+if(num>99&&num<1000)
+{
+    int lastNum=num%10;
+    Console.WriteLine("Последнее число "+num+" это "+lastNum);
+}
+
+
